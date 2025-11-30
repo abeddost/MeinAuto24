71832e478a9befcb5ab7b56cdeeb98c84b4b68f4
Abdul Qader Dost <abdulqaderdost@yahoo.com>
2025-11-30 12:47:09 +0100

    Fix Vercel deployment: Update to modern configuration

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -8,9 +9,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, '../public')));
+
 // Routes
 const apiRoutes = require('../routes/api');
 app.use('/api', apiRoutes);
+
+// Serve HTML pages
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/impressum', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/impressum.html'));
+});
+
+app.get('/datenschutz', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/datenschutz.html'));
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
